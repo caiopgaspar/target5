7e88d98298124a2881ab85a832673f255f2d7073
caiopgaspar <c-gaspar@hotmail.com>
2023-04-03 10:02:23 -0300

    resolucao

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("A string será invertida");
+        
+        string textoNormal = "";
+        while (textoNormal == string.Empty){
+            Console.Write("Favor digitar a string: ");
+            textoNormal = Console.ReadLine();
+        }
+
+        string textoInvertido = "";        
+        for (int i = textoNormal.Length - 1; i >= 0; i--){
+            textoInvertido += textoNormal[i];
+        }
+
+        Console.WriteLine($"String digitada: {textoNormal}");
+        Console.WriteLine($"String invertida: {textoInvertido}");
+        
+    }  
+}
